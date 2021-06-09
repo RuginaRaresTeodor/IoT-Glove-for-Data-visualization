3a1bf8440a066b7439f63f7884dcb0c7adf63819
RuginaRaresTeodor <raresrugina@gmail.com>
2021-06-09 18:24:04 +0300

    Initial commit

---
+++ static/scripts/editor.js
@@ -0,0 +1,12 @@
+function editorFunction() {
+  var x =document.getElementById("home");
+  var y =document.getElementById("theory");
+  var z =document.getElementById("build");
+  var q =document.getElementById("controls");
+  var a =document.getElementById("editor");
+  a.style.display = "block";
+  x.style.display = "none";
+  y.style.display = "none";
+  z.style.display = "none";
+  q.style.display = "none";
+}
